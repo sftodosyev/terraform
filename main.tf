96a1b42e8ce2fa5405fc6fcea7be984874dd4174
sftodosyev <s.ftodosyev@outlook.com>
2021-09-28 08:16:11 -0400

    Auto scalable goup

--- main.tf
+++ main.tf
@@ -2,18 +2,41 @@ provider "aws" {
     region = "us-east-2"
     
 }
-resource "aws_instance" "example"{
-    ami = "ami-0c55b159cbfafe1f0"
+resource "aws_launch_configuration" "example" {
+    image_id = "ami-0c55b159cbfafe1f0"
     instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.instance.id]
+    security_groups = [aws_security_group.instance.id]
     user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, World" > index.html
                 nohup busybox httpd -f -p ${var.server_port} &
                 EOF
-    tags = {
-        Name = "terraform example"
+    lifecycle {
+        create_before_destroy = true
     }
+
+}
+
+
+resource "aws_autoscaling_group" "example" {
+    launch_configuration = aws_launch_configuration.example.name
+    vpc_zone_identifier = data.aws_subnet_ids.default.ids
+        min_size = 2
+        max_size = 10
+
+        tag {
+            key = "Name"
+            value = "terraform-asg-example"
+            propagate_at_launch = true
+}
+}
+
+data "aws_vpc" "default" {
+    default = true
+}
+
+data "aws_subnet_ids" "default" {
+    vpc_id = data.aws_vpc.default.id
 }
 
 variable "server_port" {
@@ -31,9 +54,4 @@ resource "aws_security_group" "instance" {
             protocol = "tcp"
             cidr_blocks = ["0.0.0.0/0"]
 }
-}
-
-output "public_ip" {
-    value = aws_instance.example.public_ip
-    description = "The public IP address of the web server"
 }
