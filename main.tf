8648f2a58c5f048195f7e5d72dc98fd8d8c1fbaa
sftodosyev <s.ftodosyev@outlook.com>
2021-10-08 06:56:20 -0400

    08.10.2021

--- main.tf
+++ main.tf
@@ -21,6 +21,8 @@ resource "aws_launch_configuration" "example" {
 resource "aws_autoscaling_group" "example" {
     launch_configuration = aws_launch_configuration.example.name
     vpc_zone_identifier = data.aws_subnet_ids.default.ids
+    target_group_arns = [aws_lb_target_group.asg.arn]
+        health_check_type = "ELB"
         min_size = 2
         max_size = 10
 
@@ -30,6 +32,7 @@ resource "aws_autoscaling_group" "example" {
             propagate_at_launch = true
 }
 }
+
 
 data "aws_vpc" "default" {
     default = true
@@ -53,5 +56,81 @@ resource "aws_security_group" "instance" {
             to_port = var.server_port
             protocol = "tcp"
             cidr_blocks = ["0.0.0.0/0"]
+    }
 }
+
+resource "aws_security_group" "alb" {
+    name = "terraform-example-alb"
+    # Разрешаем все входящие HTTP-запросы
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    } 
+    #Разрешаем все исходящие запросы
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_lb" "example" {
+    name = "terraform-asg-example"
+    load_balancer_type = "application"
+    subnets = data.aws_subnet_ids.default.ids
+    security_groups = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+    load_balancer_arn = aws_lb.example.arn
+    port = 80
+    protocol = "HTTP"
+    # По умолчанию возвращает простую страницу с кодом 404
+        default_action {
+            type = "fixed-response"
+        fixed_response {
+            content_type = "text/plain"
+            message_body = "404: page not found"
+            status_code = 404
+        }
+        }
+}
+
+resource "aws_lb_target_group" "asg" {
+    name = "terraform-asg-example"
+    port = var.server_port
+    protocol = "HTTP"
+    vpc_id = data.aws_vpc.default.id
+    health_check {
+        path = "/"
+        protocol = "HTTP"
+        matcher = "200"
+        interval = 15
+        timeout = 3
+        healthy_threshold = 2
+        unhealthy_threshold = 2
+    }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+    listener_arn = aws_lb_listener.http.arn
+    priority = 100
+    condition {
+        path_pattern {
+            values = ["*"]
+        }
+    } 
+
+    action {
+        type = "forward"
+        target_group_arn = aws_lb_target_group.asg.arn
+    }
+}
+
+output "alb_dns_name" {
+    value = aws_lb.example.dns_name
+    description = "The domain name of the load balancer"
 }
